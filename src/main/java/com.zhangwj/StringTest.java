203e9eb743df952bdb158ad3fb800cc90fdca3eb
zhangwj11 <zhangwj1205@qq.com>
2020-11-19 14:03:36 +0800

    String示例

---
+++ src/main/java/com.zhangwj/StringTest.java
@@ -0,0 +1,13 @@
+package com.zhangwj;
+
+/**
+ * @Description:
+ * @author: ZhangWJ
+ * @date: 2020年11月18日 15:49
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        //创建了多少个对象？
+        String str2 = new String("ABC") + "ABC" ;
+    }
+}
